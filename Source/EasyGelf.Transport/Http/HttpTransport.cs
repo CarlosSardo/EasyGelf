85e899f5c4faca0f72719634490b31fcb06ad411
carloss <carlos.sardo@tamtam.nl>
2015-04-01 16:10:29 +0200

    event logging in windows, in case send fails

--- Source/EasyGelf.Transport/Http/HttpTransport.cs
+++ Source/EasyGelf.Transport/Http/HttpTransport.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using EasyGelf.Core;
 using System;
+using System.Diagnostics;
 
 namespace EasyGelf.Transport.Http {
     public sealed class HttpTransport : ITransport {
@@ -14,29 +15,36 @@ namespace EasyGelf.Transport.Http {
         }
 
         public void Send(GelfMessage message) {
+            try {
+                var request = (HttpWebRequest)WebRequest.Create(configuration.ConnectionUri);
+                request.Method = "POST";
+                request.ReadWriteTimeout = request.Timeout = configuration.Timeout;
+                request.ServicePoint.Expect100Continue = false; // See http://blogs.msdn.com/shitals/archive/2008/12/27/9254245.aspx
 
-            var request = (HttpWebRequest)WebRequest.Create(configuration.ConnectionUri);
-            request.Method = "POST";
-            request.ReadWriteTimeout = request.Timeout = configuration.Timeout;
-            request.ServicePoint.Expect100Continue = false; // See http://blogs.msdn.com/shitals/archive/2008/12/27/9254245.aspx
+                // Basic Authentication Support
+                // In case Graylog2 Server is behind a Firewall/Proxy/Load-Balancer, you can make use of Basic Auth (username+password)
+                if (configuration.HasBasicAuthenticationSettings) {
+                    request.PreAuthenticate = true;
+                    request.Credentials = configuration.GetCredentialCache();
+                }
 
-            // Basic Authentication Support
-            // In case Graylog2 Server is behind a Firewall/Proxy/Load-Balancer, you can make use of Basic Auth (username+password)
-            if (configuration.HasBasicAuthenticationSettings) {
-                request.PreAuthenticate = true;
-                request.Credentials = configuration.GetCredentialCache();
+                using (var requestStream = request.GetRequestStream())
+                using (var messageStream = new MemoryStream(messageSerializer.Serialize(message)))
+                    messageStream.CopyTo(requestStream);
+
+                using (var response = (HttpWebResponse)request.GetResponse()) {
+                    if (response.StatusCode == HttpStatusCode.Accepted)
+                        return;
+
+                    throw new SendFailedException();
+                }
+            } catch (Exception e) {
+                if (e != null) {
+                    Trace.WriteLine(e);
+                    throw;
+                }
             }
-
-            using (var requestStream = request.GetRequestStream())
-            using (var messageStream = new MemoryStream(messageSerializer.Serialize(message)))
-                messageStream.CopyTo(requestStream);
-
-            using (var response = (HttpWebResponse)request.GetResponse()) {
-                if (response.StatusCode == HttpStatusCode.Accepted)
-                    return;
-
-                throw new SendFailedException();
-            }
+            
         }
 
         public void Close() {
